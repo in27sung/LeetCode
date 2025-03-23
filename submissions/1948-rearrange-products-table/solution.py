8bacfd124062a70bcccd0b20be9373bec1442a87
Insung <110476794+in27sung@users.noreply.github.com>
2025-03-23 03:59:23 +0000

    [LeetCode Sync] Runtime - 500 ms (62.60%), Memory - 67 MB (22.71%)

---
+++ submissions/1948-rearrange-products-table/solution.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    return products.melt(id_vars='product_id', value_name='price', var_name='store').dropna().sort_values(by=['product_id'])
