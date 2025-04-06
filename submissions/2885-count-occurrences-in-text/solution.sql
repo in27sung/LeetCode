c2ca55136d43cda829ab9ff999ae2e6a3bef5cad
Insung <110476794+in27sung@users.noreply.github.com>
2025-04-06 03:32:40 +0000

    [LeetCode Sync] Runtime - 312 ms (81.71%), Memory - 0.0B (100.00%)

---
+++ submissions/2885-count-occurrences-in-text/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    'bull' AS word,
+    SUM(content LIKE '% bull %') AS count
+FROM
+    Files
+UNION ALL
+SELECT
+    'bear' AS word,
+    SUM(content LIKE '% bear %') AS count
+FROM
+    Files
